c3a3eec5c02f4fade38cb07a5842da4037a66bae
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2021-04-28 01:45:55 +0200

    Play around with diskdb

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+const diskdb = require('diskdb');
+
+const db = diskdb.connect('databases/', ['submissions']);
+
+interface submission {
+  site: string
+  filepath: any // todo
+  username: string
+  description: string
+  date: any // todo
+}
+
+const sub: submission = {
+  site: "FA",
+  filepath: null,
+  username: "babystar",
+  description: "some art of Star I guess",
+  date: "today"
+}
+
+db.submissions.save(sub)
